ba6cb4d9830c43b214d97b1a6c83b1320b0420df
wjxing <lbxx_cute@163.com>
2019-12-07 16:03:10 +0800

    Modify

--- mak/modules/setup_link.mk
+++ mak/modules/setup_link.mk
@@ -9,6 +9,7 @@ all:
 	$(q) rm -rf $(HOME)/.vim && ln -s $(TOOLBOX_HOME)/vimplugin $(HOME)/.vim
 	$(q) rm -rf $(HOME)/.oh-my-zsh && ln -s $(TOOLBOX_SRC_DIR)/zsh-plugin/oh-my-zsh $(HOME)/.oh-my-zsh
 	$(q) rm -rf $(TOOLBOX_SRC_DIR)/zsh-plugin/oh-my-zsh/plugins/zsh-autosuggestions && ln -s $(TOOLBOX_SRC_DIR)/zsh-plugin/zsh-autosuggestions $(TOOLBOX_SRC_DIR)/zsh-plugin/oh-my-zsh/plugins/zsh-autosuggestions
+	$(q) rm -rf $(TOOLBOX_SRC_DIR)/zsh-plugin/oh-my-zsh/themes/powerlevel10k && ln -s $(TOOLBOX_SRC_DIR)/zsh-theme/powerlevel10k $(TOOLBOX_SRC_DIR)/zsh-plugin/oh-my-zsh/themes/powerlevel10k
 	$(q) rm -rf $(HOME)/.bashrc && ln -s $(RCS_DIR)/_bashrc $(HOME)/.bashrc
 	$(q) rm -rf $(HOME)/.zshrc && ln -s $(RCS_DIR)/_zshrc $(HOME)/.zshrc
 	$(q) rm -rf $(HOME)/.shrc &&  ln -s $(RCS_DIR)/_shrc $(HOME)/.shrc
@@ -17,4 +18,4 @@ all:
 	$(q) rm -rf $(HOME)/.p10k.zsh && ln -s $(RCS_DIR)/_p10k.zsh $(HOME)/.p10k.zsh
 	$(q) rm -rf $(HOME)/.screenrc && ln -s $(RCS_DIR)/_screenrc $(HOME)/.screenrc
 	$(q) rm -rf $(HOME)/.tmux.conf && ln -s $(RCS_DIR)/_tmux.conf $(HOME)/.tmux.conf
-	$(q) cd $(TOOLBOX_SRC_DIR)/zsh-plugin/autojump/ && ./install.py --destdir $(TOOLBOX_INSTALL)
+	$(q) cd $(TOOLBOX_SRC_DIR)/zsh-plugin/autojump/ && ./install.py
