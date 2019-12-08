fb806b7d5382defb5a6856bdac6041e5e4b66888
wjxing <lbxx_cute@163.com>
2019-12-08 16:11:34 +0800

    Use macro.

--- mak/definitions.mk
+++ mak/definitions.mk
@@ -3,3 +3,7 @@ $(q) mkdir -p $(dir $@)
 $(q) rm -f $@
 $(q) cp $< $@
 endef
+
+define link-file-to-target
+$(q) [ ! -f $(1) || -L $(1) ] && rm $(1) && ln -s $(2) $(1)
+endef
--- mak/modules/setup_link.mk
+++ mak/modules/setup_link.mk
@@ -10,13 +10,13 @@ all:
 	$(q) rm -rf $(HOME)/.oh-my-zsh && ln -s $(TOOLBOX_SRC_DIR)/zsh-plugin/oh-my-zsh $(HOME)/.oh-my-zsh
 	$(q) rm -rf $(TOOLBOX_SRC_DIR)/zsh-plugin/oh-my-zsh/plugins/zsh-autosuggestions && ln -s $(TOOLBOX_SRC_DIR)/zsh-plugin/zsh-autosuggestions $(TOOLBOX_SRC_DIR)/zsh-plugin/oh-my-zsh/plugins/zsh-autosuggestions
 	$(q) rm -rf $(TOOLBOX_SRC_DIR)/zsh-plugin/oh-my-zsh/themes/powerlevel10k && ln -s $(TOOLBOX_SRC_DIR)/zsh-theme/powerlevel10k $(TOOLBOX_SRC_DIR)/zsh-plugin/oh-my-zsh/themes/powerlevel10k
-	$(q) rm -rf $(HOME)/.bashrc && ln -s $(RCS_DIR)/_bashrc $(HOME)/.bashrc
-	$(q) rm -rf $(HOME)/.zshrc && ln -s $(RCS_DIR)/_zshrc $(HOME)/.zshrc
-	$(q) rm -rf $(HOME)/.shrc &&  ln -s $(RCS_DIR)/_shrc $(HOME)/.shrc
-	$(q) rm -rf $(HOME)/.gitconfig && ln -s $(RCS_DIR)/_gitconfig $(HOME)/.gitconfig
-	$(q) rm -rf $(HOME)/.gdbinit && ln -s $(RCS_DIR)/_gdbinit $(HOME)/.gdbinit
-	$(q) rm -rf $(HOME)/.inputrc && ln -s $(RCS_DIR)/_inputrc $(HOME)/.inputrc
-	$(q) rm -rf $(HOME)/.p10k.zsh && ln -s $(RCS_DIR)/_p10k.zsh $(HOME)/.p10k.zsh
-	$(q) rm -rf $(HOME)/.screenrc && ln -s $(RCS_DIR)/_screenrc $(HOME)/.screenrc
-	$(q) rm -rf $(HOME)/.tmux.conf && ln -s $(RCS_DIR)/_tmux.conf $(HOME)/.tmux.conf
+	$(q) $(link-file-to-target $(RCS_DIR)/_bashrc,$(HOME)/.bashrc)
+	$(q) $(link-file-to-target $(RCS_DIR)/_zshrc,$(HOME)/.zshrc)
+	$(q) $(link-file-to-target $(RCS_DIR)/_shrc,$(HOME)/.shrc)
+	$(q) $(link-file-to-target $(RCS_DIR)/_gitconfig,$(HOME)/.gitconfig)
+	$(q) $(link-file-to-target $(RCS_DIR)/_gdbinit,$(HOME)/.gdbinit)
+	$(q) $(link-file-to-target $(RCS_DIR)/_inputrc,$(HOME)/.inputrc)
+	$(q) $(link-file-to-target $(RCS_DIR)/_p10k.zsh,$(HOME)/.p10k.zsh)
+	$(q) $(link-file-to-target $(RCS_DIR)/_screenrc,$(HOME)/.screenrc)
+	$(q) $(link-file-to-target $(RCS_DIR)/_tmux.conf,$(HOME)/.tmux.conf)
 	$(q) cd $(TOOLBOX_SRC_DIR)/zsh-plugin/autojump/ && ./install.py
