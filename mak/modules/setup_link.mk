f271f872b5465243f03c615fbb766d7e073a83df
wjxing <lbxx_cute@163.com>
2019-12-08 15:47:30 +0800

    Add gdbinit.

--- mak/modules/setup_link.mk
+++ mak/modules/setup_link.mk
@@ -14,6 +14,7 @@ all:
 	$(q) rm -rf $(HOME)/.zshrc && ln -s $(RCS_DIR)/_zshrc $(HOME)/.zshrc
 	$(q) rm -rf $(HOME)/.shrc &&  ln -s $(RCS_DIR)/_shrc $(HOME)/.shrc
 	$(q) rm -rf $(HOME)/.gitconfig && ln -s $(RCS_DIR)/_gitconfig $(HOME)/.gitconfig
+	$(q) rm -rf $(HOME)/.gdbinit && ln -s $(RCS_DIR)/_gdbinit $(HOME)/.gdbinit
 	$(q) rm -rf $(HOME)/.inputrc && ln -s $(RCS_DIR)/_inputrc $(HOME)/.inputrc
 	$(q) rm -rf $(HOME)/.p10k.zsh && ln -s $(RCS_DIR)/_p10k.zsh $(HOME)/.p10k.zsh
 	$(q) rm -rf $(HOME)/.screenrc && ln -s $(RCS_DIR)/_screenrc $(HOME)/.screenrc
